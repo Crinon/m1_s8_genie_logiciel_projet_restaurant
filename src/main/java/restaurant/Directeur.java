441335e60f4e53a05fbda50e4a1ffae1674d6ea9
Nicolas Crinon <48944000+Crinon@users.noreply.github.com>
2021-05-22 17:12:52 +0200

    Suppression attribut numTable de commande.java

--- src/main/java/restaurant/Directeur.java
+++ src/main/java/restaurant/Directeur.java
@@ -7,190 +7,182 @@ import java.util.HashMap;
 
 public class Directeur extends Personne {
 
-    /**
-     * @param id
-     * @param nom
-     * @param identifiant
-     */
-    public Directeur(int id, String nom, String identifiant) {
-	super(id, nom, identifiant);
-    }
-
-    @Override
-    public String toString() {
-	return "Directeur [id=" + id + ", nom=" + nom + ", identifiant=" + identifiant + "]";
-    }
-
-    public Personne ajouterPersonnel(String nom, String role, ArrayList<Personne> personnel) {
-	Personne personne = null;
-	// Objet pour intéragir avec la base de données
-	try {
-	    Sql sql = new Sql();
-	    // Utilisation de la requête pour insérer un personnel
-	    personne = sql.ajouterPersonne(nom, role);
-	    personnel.add(personne);
+	/**
+	 * @param id
+	 * @param nom
+	 * @param identifiant
+	 */
+	public Directeur(int id, String nom, String identifiant) {
+		super(id, nom, identifiant);
 	}
-	catch (ClassNotFoundException | SQLException | IOException e) {
-	    e.printStackTrace();
+
+	@Override
+	public String toString() {
+		return "Directeur [id=" + id + ", nom=" + nom + ", identifiant=" + identifiant + "]";
 	}
-	return personne;
-    }
 
-    public Personne modifierPersonnel(Personne personne, String role) {
-	// Objet pour intéragir avec la base de données
-	try {
-	    Sql sql = new Sql();
-	    // Utilisation de la requête pour insérer un serveur
-	    sql.modifierPersonne(personne, role);
-	    Personne newPersonne;
-	    switch (role) {
-		case "assistant":
-		    newPersonne = new Assistant(personne.getId(), personne.getNom(), personne.getIdentifiant());
-		break;
-
-		case "serveur":
-		    newPersonne = new Serveur(personne.getId(), personne.getNom(), personne.getIdentifiant());
-		break;
-
-		case "maitrehotel":
-		    newPersonne = new Maitrehotel(personne.getId(), personne.getNom(), personne.getIdentifiant());
-		break;
-
-		case "directeur":
-		    newPersonne = new Directeur(personne.getId(), personne.getNom(), personne.getIdentifiant());
-		break;
-
-		case "cuisinier":
-		    newPersonne = new Cuisinier(personne.getId(), personne.getNom(), personne.getIdentifiant());
-		break;
-
-		default:
-		    newPersonne = null;
-		break;
-	    }
-	    personne = newPersonne;
+	public Personne ajouterPersonnel(String nom, String role, ArrayList<Personne> personnel) {
+		Personne personne = null;
+		// Objet pour intéragir avec la base de données
+		try {
+			Sql sql = new Sql();
+			// Utilisation de la requête pour insérer un personnel
+			personne = sql.ajouterPersonne(nom, role);
+			personnel.add(personne);
+		} catch (ClassNotFoundException | SQLException | IOException e) {
+			e.printStackTrace();
+		}
+		return personne;
 	}
-	catch (ClassNotFoundException | SQLException | IOException e) {
-	    e.printStackTrace();
+
+	public Personne modifierPersonnel(Personne personne, String role) {
+		// Objet pour intéragir avec la base de données
+		try {
+			Sql sql = new Sql();
+			// Utilisation de la requête pour insérer un serveur
+			sql.modifierPersonne(personne, role);
+			Personne newPersonne;
+			switch (role) {
+			case "assistant":
+				newPersonne = new Assistant(personne.getId(), personne.getNom(), personne.getIdentifiant());
+				break;
+
+			case "serveur":
+				newPersonne = new Serveur(personne.getId(), personne.getNom(), personne.getIdentifiant());
+				break;
+
+			case "maitrehotel":
+				newPersonne = new Maitrehotel(personne.getId(), personne.getNom(), personne.getIdentifiant());
+				break;
+
+			case "directeur":
+				newPersonne = new Directeur(personne.getId(), personne.getNom(), personne.getIdentifiant());
+				break;
+
+			case "cuisinier":
+				newPersonne = new Cuisinier(personne.getId(), personne.getNom(), personne.getIdentifiant());
+				break;
+
+			default:
+				newPersonne = null;
+				break;
+			}
+			personne = newPersonne;
+		} catch (ClassNotFoundException | SQLException | IOException e) {
+			e.printStackTrace();
+		}
+		return personne;
 	}
-	return personne;
-    }
 
-    public void supprimerPersonnel(Personne personne, ArrayList<Personne> personnel) {
-	try {
-	    // Objet pour intéragir avec la base de données
-	    Sql sql = new Sql();
-	    sql.supprimerPersonne(personne);
-	    personnel.remove(personne);
+	public void supprimerPersonnel(Personne personne, ArrayList<Personne> personnel) {
+		try {
+			// Objet pour intéragir avec la base de données
+			Sql sql = new Sql();
+			sql.supprimerPersonne(personne);
+			personnel.remove(personne);
+		} catch (ClassNotFoundException | SQLException | IOException e) {
+			e.printStackTrace();
+		}
 	}
-	catch (ClassNotFoundException | SQLException | IOException e) {
-	    e.printStackTrace();
+
+	public boolean ajouterEtage() throws ClassNotFoundException, SQLException, IOException {
+		Sql sql = new Sql();
+		sql.insererEtage();
+		Restaurant.addEtage(new Etage(sql.demanderDernierId("etage"), sql.demanderDernierEtage()));
+		return true;
 	}
-    }
 
-    public boolean ajouterEtage() throws ClassNotFoundException, SQLException, IOException {
-	Sql sql = new Sql();
-	sql.insererEtage();
-	Restaurant.addEtage(new Etage(sql.demanderDernierId("etage"), sql.demanderDernierEtage()));
-	return true;
-    }
-
-    public void supprimerDernierEtage() throws ClassNotFoundException, SQLException, IOException {
-	Sql sql = new Sql();
-	sql.supprimerEtage();
-	Restaurant.getEtages().remove(Restaurant.getEtages().size() - 1);
-    }
-
-    public void ajouterTable(int numero, int capacite, Etage etage)
-	    throws ClassNotFoundException, SQLException, IOException {
-	Sql sql = new Sql();
-	sql.insererTable(numero, capacite, etage);
-	etage.addTable(new Table(sql.demanderDernierId("tables"), capacite, numero, EtatTable.Libre));
-    }
-
-    public void modifierNumeroTable(Table table, int newNumero)
-	    throws ClassNotFoundException, SQLException, IOException {
-	boolean success;
-	Sql sql = new Sql();
-	success = sql.updateTable(table.getNumero(), newNumero, table);
-	if (success) {
-	    table.setNumero(newNumero);
+	public void supprimerDernierEtage() throws ClassNotFoundException, SQLException, IOException {
+		Sql sql = new Sql();
+		sql.supprimerEtage();
+		Restaurant.getEtages().remove(Restaurant.getEtages().size() - 1);
 	}
-    }
 
-    public void supprimerTable(int indexToRemove, ArrayList<Table> tables)
-	    throws ClassNotFoundException, SQLException, IOException {
-	boolean success;
-	Sql sql = new Sql();
-	success = sql.deleteTable(tables.get(indexToRemove));
-	if (success) {
-	    tables.remove(indexToRemove);
+	public void ajouterTable(int numero, int capacite, Etage etage)
+			throws ClassNotFoundException, SQLException, IOException {
+		Sql sql = new Sql();
+		sql.insererTable(numero, capacite, etage);
+		etage.addTable(new Table(sql.demanderDernierId("tables"), capacite, numero, EtatTable.Libre));
 	}
-    }
 
-    public boolean ajouterIngredient(String nom, ArrayList<Ingredient> ingredients) {
-	boolean success = false;
-	Sql sql;
-	try {
-	    sql = new Sql();
-	    success = sql.insererIngredient(nom);
-	    if (success) {
-		ingredients.add(new Ingredient(sql.demanderDernierId("ingredient"), nom, 0));
+	public void modifierNumeroTable(Table table, int newNumero)
+			throws ClassNotFoundException, SQLException, IOException {
+		boolean success;
+		Sql sql = new Sql();
+		success = sql.updateTable(table.getNumero(), newNumero, table);
+		if (success) {
+			table.setNumero(newNumero);
+		}
+	}
+
+	public void supprimerTable(Table tableToremove, ArrayList<Table> tables)
+			throws ClassNotFoundException, SQLException, IOException {
+		boolean success;
+		Sql sql = new Sql();
+		success = sql.deleteTable(tableToremove);
+		if (success) {
+			tables.remove(tableToremove);
+		}
+	}
+
+	public boolean ajouterIngredient(String nom, ArrayList<Ingredient> ingredients) {
+		boolean success = false;
+		Sql sql;
+		try {
+			sql = new Sql();
+			success = sql.insererIngredient(nom);
+			if (success) {
+				ingredients.add(new Ingredient(sql.demanderDernierId("ingredient"), nom, 0));
+				return success;
+			} else {
+				System.out.println("L'ajout de l'ingrédient " + nom + " a échoué.");
+				return false;
+			}
+		} catch (ClassNotFoundException | SQLException | IOException e) {
+			e.printStackTrace();
+		}
 		return success;
-	    }
-	    else {
-		System.out.println("L'ajout de l'ingrédient " + nom + " a échoué.");
-		return false;
-	    }
 	}
-	catch (ClassNotFoundException | SQLException | IOException e) {
-	    e.printStackTrace();
-	}
-	return success;
-    }
 
-    public void commanderIngredient(Ingredient ingredient, int ajout)
-	    throws ClassNotFoundException, SQLException, IOException {
-	Sql sql = new Sql();
-	sql.commanderIngredient(ingredient, ajout);
-    }
+	public void commanderIngredient(Ingredient ingredient, int ajout)
+			throws ClassNotFoundException, SQLException, IOException {
+		Sql sql = new Sql();
+		sql.commanderIngredient(ingredient, ajout);
+	}
 
-    public Plat creerPlat(String nom, Double prixPlat, int tempsPrepa, boolean surCarte, Type type, Categorie categorie,
-	    HashMap<Ingredient, Integer> ingredientQuantite) {
-	Sql sql;
-	try {
-	    sql = new Sql();
-	    Plat plat = sql.insererPlat(nom, prixPlat, tempsPrepa, surCarte, type, categorie, ingredientQuantite);
-	    Restaurant.getPlats().add(plat);
-	    return plat;
+	public Plat creerPlat(String nom, Double prixPlat, int tempsPrepa, boolean surCarte, Type type, Categorie categorie,
+			HashMap<Ingredient, Integer> ingredientQuantite) {
+		Sql sql;
+		try {
+			sql = new Sql();
+			Plat plat = sql.insererPlat(nom, prixPlat, tempsPrepa, surCarte, type, categorie, ingredientQuantite);
+			Restaurant.getPlats().add(plat);
+			return plat;
+		} catch (ClassNotFoundException | SQLException | IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
-	catch (ClassNotFoundException | SQLException | IOException e) {
-	    e.printStackTrace();
-	}
-	return null;
-    }
 
-    public void modifierPlat(Plat plat) {
-	Sql sql;
-	try {
-	    sql = new Sql();
-	    sql.modifierPlat(plat);
+	public void modifierPlat(Plat plat) {
+		Sql sql;
+		try {
+			sql = new Sql();
+			sql.modifierPlat(plat);
+		} catch (ClassNotFoundException | SQLException | IOException e) {
+			e.printStackTrace();
+		}
 	}
-	catch (ClassNotFoundException | SQLException | IOException e) {
-	    e.printStackTrace();
-	}
-    }
 
-    public void supprimerPlat(Plat plat) {
-	Sql sql;
-	try {
-	    sql = new Sql();
-	    sql.supprimerPlat(plat);
-	    Restaurant.getPlats().remove(plat);
+	public void supprimerPlat(Plat plat) {
+		Sql sql;
+		try {
+			sql = new Sql();
+			sql.supprimerPlat(plat);
+			Restaurant.getPlats().remove(plat);
+		} catch (ClassNotFoundException | SQLException | IOException e) {
+			e.printStackTrace();
+		}
 	}
-	catch (ClassNotFoundException | SQLException | IOException e) {
-	    e.printStackTrace();
-	}
-    }
 
 }
--- src/test/java/m1_s8_genie_logiciel_projet_restaurant/TestUnitaire.java
+++ src/test/java/m1_s8_genie_logiciel_projet_restaurant/TestUnitaire.java
@@ -491,7 +491,7 @@ public class TestUnitaire {
 			// La table créée et a supprimée
 			Table tableActuelle = etage.getTables().get(0);
 			// On supprime la table
-			directeur.supprimerTable(0, etage.getTables());
+			directeur.supprimerTable(tableActuelle, etage.getTables());
 			ResultSet resultSet = sql.executerSelect("SELECT numero FROM restaurant.tables WHERE id="+tableActuelle.getId());
 			// On vérifie qu'aucune ligne n'est trouvée car l'id recherché a été supprimé
 			assertFalse(resultSet.next());
@@ -517,7 +517,7 @@ public class TestUnitaire {
 			// La table créée et a supprimée
 			Table tableActuelle = etage.getTables().get(0);
 			// On supprime la table
-			directeur.supprimerTable(0, etage.getTables());
+			directeur.supprimerTable(tableActuelle, etage.getTables());
 			int totalTablesApresSuppression = etage.getTables().size();
 			// On vérifie qu'aucune ligne n'est trouvée car l'id recherché a été supprimé
 			assertTrue(totalTablesApresSuppression < totalTablesAvantSuppression);
