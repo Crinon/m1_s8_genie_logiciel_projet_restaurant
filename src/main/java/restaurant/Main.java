269fc4099b37284c57a8dd30074f2a965894b8ed
flzhm <45990224+flozhm@users.noreply.github.com>
2021-05-23 22:33:50 +0200

    Directeur : menu + fonctionnalité  commander un ingrédient

--- src/main/java/restaurant/Main.java
+++ src/main/java/restaurant/Main.java
@@ -1,20 +1,32 @@
 package restaurant;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Main {
 
 	private static Scanner scanner = new Scanner(System.in);
+	static boolean quitterApplication = false;
+	static Personne persConnectee = null;
 
+	
+	public static void quitterApplication() {
+		quitterApplication = true; //Permet de quitter l'application
+		System.out.println("\nAu revoir et à bientôt !");
+	}
+	
 	// Permet de se connecter via l'identifiant, s'il existe
 	public static Personne connexion() {
 
 		// Requête qui vérifie
 		try {
 
-			System.out.println("Veuillez saisir votre identifiant");
+			System.out.println("Veuillez saisir 0 pour quitter l'application,\n ou votre identifiant pour vous connecter.");
 			String identifiant = scanner.nextLine();
+			
+			if (!estNulleOuVide(identifiant) && identifiant.equals("0")) {
+				quitterApplication();
+				return null;
+			}
 
 			// On vérifie si l'identifiant entré existe
 			for (int i = 0; i < Restaurant.getPersonnel().size(); i++) {
@@ -23,15 +35,15 @@ public class Main {
 				}
 			}
 		} catch (Exception e) {
-			System.out.println("Cet utilisatuer n'existe pas, veuillez réessayer");
+			//
 		}
-
+		System.out.println("Cet utilisateur n'existe pas, veuillez réessayer");
 		return null;
 	}
 
 	// Vérifie la valeur entree au clavier
-	public static boolean valeurIntOk(int valeur, int min, int max) {
-		if (valeur < min || valeur > max) {
+	public static boolean valeurIntOk(int valeur, int max) {
+		if (valeur < 0 || valeur > max) {
 			return false;
 		}
 		return true;
@@ -46,7 +58,7 @@ public class Main {
 	}
 
 	// Vérifie que la chaine de caractères n'est pas nulle ni vide
-	public static boolean estNulleOuVideString(String valeur) {
+	public static boolean estNulleOuVide(String valeur) {
 
 		if (valeur == null || valeur == "") {
 			return true;
@@ -58,50 +70,121 @@ public class Main {
 	// On n'accepte pas les caractères spéciaux (accents, etc)
 	public static boolean uniquementLettres(String valeur) {
 
-		if (Pattern.matches("[a-zA-Z]+", valeur) == true) {
-			return false;
+		if (valeur.matches("[a-zA-Z]+") == true) {
+			return true;
 		}
-		return true;
+		return false;
+	}
+
+	// Méthode qui permet de vérifier que "valeur" ne contient que des chiffres
+	public static boolean uniquementChiffres(String valeur) {
+
+		if (valeur.matches("[0-9]+") == true) {
+			return true;
+		}
+		return false;
+	}
+
+	// Méthode qui permet de vérifier l'entrée pour un choix de l'utilisateur
+	public static int choixUtilisateur(int valeurChoixMax) {
+
+		String choix = "";
+
+		
+		while (estNulleOuVide(choix) || !uniquementChiffres(choix) || !valeurIntOk(Integer.parseInt(choix), valeurChoixMax)) {
+			System.out.println(">Veuillez saisir votre choix (valeur allant de 0 à " + valeurChoixMax + ")");
+			choix = scanner.nextLine();
+		}
+		return Integer.parseInt(choix);
+	}
+
+	// Affiche la liste des ingrédients disponibles
+	public static String listingIngredients() {
+		String liste = "";
+		for (int i = 0; i < Restaurant.getIngredients().size(); i++) {
+			liste += i + ":" + Restaurant.getIngredients().get(i).getNom()
+				   + "; quantite: " + Restaurant.getIngredients().get(i).getQuantite();
+		}
+		return liste;
+	}
+	
+	// Permet de commander un ingrédient pour l'ajouter au stock
+	public static void commanderIngredient() {
+		// Affichage menu
+		System.out.println("----------------------------------"
+					   + "\n-----Commander un ingredient------"
+				       + "\nListe des ingrédients : "
+				       + listingIngredients()
+				       + "\nVeuillez sélectionner le numéro de l'ingrédient à commander"
+				       + "\n----------------------------------\n"
+				       );
+		
+		int numIngredient = choixUtilisateur(Restaurant.getIngredients().size() );
+		
+		System.out.println("Veuillez sélectionner la quantite de " + Restaurant.getIngredients().get(numIngredient));
+		int qtIngredient = choixUtilisateur(500); //Quantite max par commande : 500
+		
+		//Ajout de l'ingrédient
+		//APPEL DE LA METHODE ajoutIngredient
+		
+		
+		//Valider ou retour
 	}
 
 	// Menu principal du directeur
-	public static void menuPrincipalDirecteur(int choix) {
+	public static void  menuPrincipalDirecteur() {
 
-		while (!valeurIntOk(choix, 0, 7)) {
-			System.out.println("Veuillez saisir votre identifiant");
-			String identifiant = scanner.nextLine();
+		// Affichage menu
+		System.out.println("----------------------------------"
+				        + "\n0: Déconnexion"
+				        + "\n1: Commander un ingredient"
+				        + "\n2: Ajouter personnel"
+				        + "\n3: Modifier personnel"
+				        + "\n4: Supprimer personnel"
+				        + "\n5: Suivi serveur"
+				        + "\n6: Statistiques"
+				        + "\n7: AJOUTER METHODES DES AUTRES ROLES"
+				        + "\n----------------------------------\n"
+				        );
 
-		}
-		switch (choix) {
+		switch (choixUtilisateur(7)) { //valeurChoixMin = 0
 
 		// Déconnexion
 		case 0:
-
+			persConnectee = null;
+			System.out.println("Déconnexion....\nVous avez été déconnecté.\n");
 			break;
+
 		// Commander un ingredient
 		case 1:
-
+			commanderIngredient();
 			break;
+
 		// Ajouter personnel
 		case 2:
 
 			break;
+
 		// Modifier personnel
 		case 3:
 
 			break;
+
 		// Supprimer personnel
 		case 4:
 
 			break;
+
 		// Suivi serveur
 		case 5:
 
 			break;
+
 		// Statistiques
 		case 6:
 
 			break;
+
 		// Méthodes des autres rôles....
 		case 7:
 
@@ -113,28 +196,24 @@ public class Main {
 
 	}
 
-	public static void main(String[] args) throws Exception {
-
-		boolean quitterApplication = false;
-		String identifiant = "";
-		Personne persConnectee = null;
+	public static void main(String[] args) throws Exception {		
 
 		// Initialisation du programme
 		Restaurant.initialisation(); // On a donc au minimum un utilisateur : le directeur
 		persConnectee = connexion();
 		// Application active
 		while (!quitterApplication) {
-
+			
 			while (persConnectee == null) {
 				// L'utilisateur se connecte : reconnaissance du rôle
 				persConnectee = connexion();
 			}
-			System.out.println("\nConnecté en tant que : " + persConnectee.getIdentifiant() + ";"
-					+ persConnectee.getClass().getName());
+			System.out.println("\nConnecté en tant que : " + persConnectee.getIdentifiant() );
 
+			// Menu en fonction du rôle
 			switch (persConnectee.getClass().getName()) {
 			case "restaurant.Directeur":
-
+				menuPrincipalDirecteur();
 				break;
 
 			case "restaurant.Maitrehotel":
