6ae0844ddaa4c7a6a8d7e1083062e44585e704b7
PIERRATCharly <31919291+PIERRATCharly@users.noreply.github.com>
2021-05-25 07:40:03 +0200

    Récupération plus petite table disponible

--- src/main/java/restaurant/Restaurant.java
+++ src/main/java/restaurant/Restaurant.java
@@ -1,54 +1,59 @@
 package restaurant;
 
 import java.io.IOException;
-import java.sql.Date;
 import java.sql.SQLException;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class Restaurant {
 
-	private static ArrayList<Etage> etages;
-	private static ArrayList<Personne> personnel;
-	private static ArrayList<Ingredient> ingredients;
-	private static ArrayList<Plat> plats;
-	private static ArrayList<Affectation> affectationsJour;
-	private static ArrayList<Reservation> reservationsJour;
-	private static ArrayList<Commande> commandes;
+    private static ArrayList<Etage>	  etages;
+    private static ArrayList<Personne>	  personnel;
+    private static ArrayList<Ingredient>  ingredients;
+    private static ArrayList<Plat>	  plats;
+    private static ArrayList<Affectation> affectationsJour;
+    private static ArrayList<Reservation> reservationsJour;
+    private static ArrayList<Commande>	  commandes;
 
-	private static LocalTime heureDejeunerOuverture;
-	private static LocalTime heureDejeunerLimite;
-	private static LocalTime heureDinerOuverture;
-	private static LocalTime heureDinerLimite;
-	private static int nbTableMax;
+    private static LocalTime heureDejeunerOuverture;
+    private static LocalTime heureDejeunerLimite;
+    private static LocalTime heureDinerOuverture;
+    private static LocalTime heureDinerLimite;
+    private static int	     nbTableMax;
 
-	public static void initialisation() {
-		Sql sql;
-		try {
-			sql = new Sql();
-			sql.initialiserHoraires();
-			sql.initialiserIngredients();
-			sql.initialiserEtages();
-			for (Etage etage : etages) {
-				etage.initialiserTables();
+    public static void initialisation() {
+	Sql sql;
+	try {
+	    sql = new Sql();
+	    sql.initialiserHoraires();
+	    sql.initialiserIngredients();
+	    sql.initialiserEtages();
+	    for (Etage etage : etages) {
+		etage.initialiserTables();
 //				toutesLesTables.addAll(etage.getTables());
-			}
-			// Ajout en base uniquement du directeur s'il n'y en a aucun dans la base
-			sql.premierDemarrage();
-			// Initialisation du personnel
-			sql.initialiserPersonnel();
-			sql.initialiserPlats();
-			// Au lancement du programme en début de journée, aucune table n'est occupée
-			affectationsJour = new ArrayList<Affectation>();
-			
-			reservationsJour = new ArrayList<Reservation>();
-			sql.initialiserReservation();
-			commandes  = new ArrayList<Commande>();
-		} catch (ClassNotFoundException | SQLException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	    }
+	    // Ajout en base uniquement du directeur s'il n'y en a aucun dans la base
+	    sql.premierDemarrage();
+	    // Initialisation du personnel
+	    sql.initialiserPersonnel();
+	    sql.initialiserPlats();
+	    // Au lancement du programme en début de journée, aucune table n'est occupée
+	    affectationsJour = new ArrayList<Affectation>();
+
+	    reservationsJour = new ArrayList<Reservation>();
+	    sql.initialiserReservation();
+	    commandes = new ArrayList<Commande>();
 	}
+	catch (ClassNotFoundException | SQLException | IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+    }
 
 //	public static void premierDemarrage(Sql sql) {
 //		// Regarde si c'est le premier démarrage de l'application
@@ -57,115 +62,153 @@ public final class Restaurant {
 //		
 //	}
 
-	// Méthode pour récupérer l'ensemble des tables du restaurant
-	public static ArrayList<Table> getToutesLesTables() {
-		ArrayList<Table> toutesLesTables = new ArrayList<>();
-		etages.forEach(etage->etage.getTables().forEach(table->toutesLesTables.add(table)));
-		return toutesLesTables;
-	}
+    // Méthode pour récupérer l'ensemble des tables du restaurant
+    public static List<Table> getToutesLesTables() {
+	List<Table> toutesLesTables = new ArrayList<Table>();
+	etages.forEach(etage -> etage.getTables().forEach(table -> toutesLesTables.add(table)));
+	return toutesLesTables;
+    }
 
-
-	public static ArrayList<Ingredient> getIngredients() {
-		return ingredients;
+    // On veut avoir la plus petite table disponible pouvant contenir tous les
+    // participants d'un repas
+    public static Table getMiniTable(int nombreParticipants, Date dateReservation) {
+	// Récupération des tables
+	List<Table> toutesLesTables = getToutesLesTables();
+	// Supression des tables deja reservees
+	reservationsJour.stream().filter(reservation -> !memeDate(reservation.getDateVenue(), dateReservation))
+		.forEach(reservation -> toutesLesTables.remove(reservation.getTable()));
+	// Filtre des tables selon la capacite et tri dans l'ordre croissant
+	List<Table> tablesPossibles = toutesLesTables.stream()
+		.filter(table -> table.getCapacite() >= nombreParticipants)
+		.sorted(Comparator.comparingInt(Table::getCapacite)).collect(Collectors.toList());
+	// Si au moins une table est disponible, on la retourne
+	if (!tablesPossibles.isEmpty()) {
+	    return tablesPossibles.get(0);
 	}
+	return null;
+    }
 
-	public static void setIngredients(ArrayList<Ingredient> ingredients) {
-		Restaurant.ingredients = ingredients;
+    private static boolean memeDate(Date dateVenue, Date dateReservation) {
+	Calendar calendarVenue = Calendar.getInstance();
+	calendarVenue.setTime(dateVenue);
+	LocalTime heureDateVenue = LocalTime.of(calendarVenue.get(Calendar.HOUR_OF_DAY),
+		calendarVenue.get(Calendar.MINUTE), calendarVenue.get(Calendar.SECOND));
+	Calendar calendarReservation = Calendar.getInstance();
+	calendarReservation.setTime(dateReservation);
+	LocalTime heureDateReservation = LocalTime.of(calendarReservation.get(Calendar.HOUR_OF_DAY),
+		calendarReservation.get(Calendar.MINUTE), calendarReservation.get(Calendar.SECOND));
+	if ((heureDateVenue.isAfter(heureDejeunerOuverture) && heureDateVenue.isBefore(heureDejeunerLimite)
+		&& heureDateReservation.isAfter(heureDejeunerOuverture)
+		&& heureDateReservation.isBefore(heureDejeunerLimite))
+		|| (heureDateVenue.isAfter(heureDinerOuverture) && heureDateVenue.isBefore(heureDinerLimite)
+			&& heureDateReservation.isAfter(heureDinerOuverture)
+			&& heureDateReservation.isBefore(heureDinerLimite))) {
+	    return true;
 	}
+	return false;
+    }
 
-	public static ArrayList<Etage> getEtages() {
-		return etages;
-	}
+    public static ArrayList<Ingredient> getIngredients() {
+	return ingredients;
+    }
 
-	public static void setEtages(ArrayList<Etage> etages) {
-		Restaurant.etages = etages;
-	}
+    public static void setIngredients(ArrayList<Ingredient> ingredients) {
+	Restaurant.ingredients = ingredients;
+    }
 
-	public static void addEtage(Etage etage) {
-		Restaurant.etages.add(etage);
-	}
+    public static ArrayList<Etage> getEtages() {
+	return etages;
+    }
 
-	public static ArrayList<Personne> getPersonnel() {
-		return personnel;
-	}
+    public static void setEtages(ArrayList<Etage> etages) {
+	Restaurant.etages = etages;
+    }
 
-	public static void setPersonnel(ArrayList<Personne> personnel) {
-		Restaurant.personnel = personnel;
-	}
+    public static void addEtage(Etage etage) {
+	Restaurant.etages.add(etage);
+    }
 
-	public static LocalTime getHeureDejeunerOuverture() {
-		return heureDejeunerOuverture;
-	}
+    public static ArrayList<Personne> getPersonnel() {
+	return personnel;
+    }
 
-	public static void setHeureDejeunerOuverture(LocalTime heureDejeunerOuverture) {
-		Restaurant.heureDejeunerOuverture = heureDejeunerOuverture;
-	}
+    public static void setPersonnel(ArrayList<Personne> personnel) {
+	Restaurant.personnel = personnel;
+    }
 
-	public static LocalTime getHeureDejeunerLimite() {
-		return heureDejeunerLimite;
-	}
+    public static LocalTime getHeureDejeunerOuverture() {
+	return heureDejeunerOuverture;
+    }
 
-	public static void setHeureDejeunerLimite(LocalTime heureDejeunerLimite) {
-		Restaurant.heureDejeunerLimite = heureDejeunerLimite;
-	}
+    public static void setHeureDejeunerOuverture(LocalTime heureDejeunerOuverture) {
+	Restaurant.heureDejeunerOuverture = heureDejeunerOuverture;
+    }
 
-	public static LocalTime getHeureDinerOuverture() {
-		return heureDinerOuverture;
-	}
+    public static LocalTime getHeureDejeunerLimite() {
+	return heureDejeunerLimite;
+    }
 
-	public static void setHeureDinerOuverture(LocalTime heureDinerOuverture) {
-		Restaurant.heureDinerOuverture = heureDinerOuverture;
-	}
+    public static void setHeureDejeunerLimite(LocalTime heureDejeunerLimite) {
+	Restaurant.heureDejeunerLimite = heureDejeunerLimite;
+    }
 
-	public static LocalTime getHeureDinerLimite() {
-		return heureDinerLimite;
-	}
+    public static LocalTime getHeureDinerOuverture() {
+	return heureDinerOuverture;
+    }
 
-	public static void setHeureDinerLimite(LocalTime heureDinerLimite) {
-		Restaurant.heureDinerLimite = heureDinerLimite;
-	}
+    public static void setHeureDinerOuverture(LocalTime heureDinerOuverture) {
+	Restaurant.heureDinerOuverture = heureDinerOuverture;
+    }
 
-	public static int getNbTableMax() {
-		return nbTableMax;
-	}
+    public static LocalTime getHeureDinerLimite() {
+	return heureDinerLimite;
+    }
 
-	public static void setNbTableMax(int nbTableMax) {
-		Restaurant.nbTableMax = nbTableMax;
-	}
+    public static void setHeureDinerLimite(LocalTime heureDinerLimite) {
+	Restaurant.heureDinerLimite = heureDinerLimite;
+    }
 
-	private Restaurant() {
-	}
+    public static int getNbTableMax() {
+	return nbTableMax;
+    }
 
-	public static ArrayList<Plat> getPlats() {
-		return plats;
-	}
+    public static void setNbTableMax(int nbTableMax) {
+	Restaurant.nbTableMax = nbTableMax;
+    }
 
-	public static void setPlats(ArrayList<Plat> plats) {
-		Restaurant.plats = plats;
-	}
+    private Restaurant() {
+    }
 
-	public static ArrayList<Affectation> getAffectationsJour() {
-		return affectationsJour;
-	}
+    public static ArrayList<Plat> getPlats() {
+	return plats;
+    }
 
-	public static void setAffectationsJour(ArrayList<Affectation> affectations) {
-		Restaurant.affectationsJour = affectations;
-	}
+    public static void setPlats(ArrayList<Plat> plats) {
+	Restaurant.plats = plats;
+    }
 
-	public static ArrayList<Reservation> getReservationsJour() {
-		return reservationsJour;
-	}
+    public static ArrayList<Affectation> getAffectationsJour() {
+	return affectationsJour;
+    }
 
-	public static void setReservationsJour(ArrayList<Reservation> reservationsJour) {
-		Restaurant.reservationsJour = reservationsJour;
-	}
+    public static void setAffectationsJour(ArrayList<Affectation> affectations) {
+	Restaurant.affectationsJour = affectations;
+    }
 
-	public static ArrayList<Commande> getCommandes() {
-		return commandes;
-	}
+    public static ArrayList<Reservation> getReservationsJour() {
+	return reservationsJour;
+    }
 
-	public static void setCommandes(ArrayList<Commande> commandes) {
-		Restaurant.commandes = commandes;
-	}
+    public static void setReservationsJour(ArrayList<Reservation> reservationsJour) {
+	Restaurant.reservationsJour = reservationsJour;
+    }
+
+    public static ArrayList<Commande> getCommandes() {
+	return commandes;
+    }
+
+    public static void setCommandes(ArrayList<Commande> commandes) {
+	Restaurant.commandes = commandes;
+    }
 
 }
