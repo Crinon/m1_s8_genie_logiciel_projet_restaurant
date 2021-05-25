0e584f7f0e41c57b4f484d26726b2c5686170268
PIERRATCharly <31919291+PIERRATCharly@users.noreply.github.com>
2021-05-25 09:16:06 +0200

    Nettoyer table

--- src/main/java/restaurant/Assistant.java
+++ src/main/java/restaurant/Assistant.java
@@ -1,9 +1,23 @@
 package restaurant;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class Assistant extends Personne {
 
     public Assistant(int id, String nom, String identifiant) {
 	super(id, nom, identifiant);
     }
 
+    public void nettoyerTable(Table table) {
+	Sql sql;
+	try {
+	    sql = new Sql();
+	    sql.modifierEtatTable(table, EtatTable.Libre);
+	    table.setEtat(EtatTable.Libre);
+	}
+	catch (ClassNotFoundException | SQLException | IOException e) {
+	    e.printStackTrace();
+	}
+    }
 }
